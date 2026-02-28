a6aa9e9985d48199876240fef21a6860ab1605e1
Sunil Maharjan <sunil.maharjan1998@gmail.com>
2026-02-27 22:46:37 -0500

    DD-5 added swagger and deployed in azure with azure sql server

---
+++ DependencyInjection/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using DockerDemo.Docker.Service;
+using DockerDemo.Docker.Interface;
+
+namespace DockerDemo.DependencyInjection
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IStoreService, StoreService>();
+            return services;
+
+        }
+    }
+}
---
+++ Docker/Service/StoreService.cs
@@ -0,0 +1,47 @@
+﻿using DockerDemo.Data;
+using DockerDemo.Docker.Interface;
+using DockerDemo.Docker.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerDemo.Docker.Service
+{
+    public class StoreService : IStoreService
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public StoreService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddStore(Store store)
+        {
+            _context.Stores.Add(store);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteStore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ActionResult<IEnumerable<Store>>> GetStores()
+        {
+            return await _context.Stores.ToListAsync();
+        }
+
+        public Task<ActionResult<IEnumerable<Store>>> GetStoreById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateStore(Store store)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
